6d0085697a390321d0b73c3bd0cf124fcc10b59d
ArnavMK <kothariarnav9@gmail.com>
2025-07-19 15:02:51 +0100

    Refactored to a struct for the server

--- server/server.go
+++ server/server.go
@@ -4,37 +4,17 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"sync"
 )
 
-
-func main() {
-
-    ln, err := net.Listen("tcp", ":8080");
-    fmt.Println("Started server at localhost :8080 tcp");
-
-    messages := make(chan string);
-
-    if err != nil {
-        fmt.Println(err);
-        return;
-    }
-
-    defer ln.Close();
-
-    go handleCentralMessageChannel(messages);
-
-    for {
-        connection, err := ln.Accept();
-        if err != nil {
-            fmt.Println(err);
-            return;
-        }
-
-        go handleConnections(connection, messages);
-    }
+type Server struct {
+	clients []net.Conn;
+	mutex sync.Mutex;
+	messageChannel chan string;
 }
 
-func handleConnections(conn net.Conn, messages chan string) {
+
+func (s *Server) handleConnections(conn net.Conn) {
 
     defer conn.Close();
     buf := make([]byte, 1024);
@@ -53,14 +33,45 @@ func handleConnections(conn net.Conn, messages chan string) {
         }
         
         // each client go-routine sends the message to the central channel.
-        messages <- string(buf[:n]);
+		s.messageChannel <- string(buf[:n]);
     }
 }
 
-func handleCentralMessageChannel(messages chan string) {
+func (s *Server) broadcastAllMessages() {
     
-    for message := range messages {
+    for message := range s.messageChannel {
         fmt.Println("Someone said: ", message);
     }
 }
 
+func main() {
+
+	listener, err := net.Listen("tcp", ":8080");
+	if err != nil {
+		fmt.Println(err);
+		return;
+	}
+
+	defer listener.Close();
+
+	server := &Server {
+		clients: make([]net.Conn, 0),
+		mutex: sync.Mutex{},
+		messageChannel: make(chan string),
+	}
+
+	go server.broadcastAllMessages();
+
+	for {
+		connection, err := listener.Accept();
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client has disconnected");
+				return;
+			}
+			return;
+		}
+
+		go server.handleConnections(connection);
+	}
+}
