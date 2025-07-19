92179f689314f7fa1711e8d27b845982569ae954
ArnavMK <kothariarnav9@gmail.com>
2025-07-19 12:14:27 +0100

    TCP server that logs the messages from any client

--- server/server.go
+++ server/server.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
 
 func main() {
-	fmt.Println("I am the server");
+
+    ln, err := net.Listen("tcp", ":8080");
+    fmt.Println("Started server at localhost :8080 tcp");
+
+    messages := make(chan string);
+
+    if err != nil {
+        fmt.Println(err);
+        return;
+    }
+
+    defer ln.Close();
+
+    go handleCentralMessageChannel(messages);
+
+    for {
+        connection, err := ln.Accept();
+        if err != nil {
+            fmt.Println(err);
+            return;
+        }
+
+        go handleConnections(connection, messages);
+    }
 }
+
+func handleConnections(conn net.Conn, messages chan string) {
+
+    defer conn.Close();
+    buf := make([]byte, 1024);
+
+    for {
+        n, err := conn.Read(buf);
+
+        if err == io.EOF {
+            fmt.Println("Client broke the connection");
+            return;
+        }
+
+        if err != nil {
+            fmt.Println(err);
+            return;
+        }
+        
+        // each client go-routine sends the message to the central channel.
+        messages <- string(buf[:n]);
+    }
+}
+
+func handleCentralMessageChannel(messages chan string) {
+    
+    for message := range messages {
+        fmt.Println("Someone said: ", message);
+    }
+}
+
